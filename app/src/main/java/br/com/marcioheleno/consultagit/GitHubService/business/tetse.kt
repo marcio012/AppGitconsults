8cbaac170b8f7a081b3f623efc5a5f88d80f86f4
alunor17 <alunor17@M72445.local>
2018-07-17 17:56:03 -0300

    ajuste dos model

--- app/src/main/java/br/com/marcioheleno/consultagit/GitHubService/business/tetse.kt
+++
@@ -1,4 +0,0 @@
-package br.com.marcioheleno.consultagit.GitHubService.business
-
-object tetse {
-}
---
+++ app/src/main/java/br/com/marcioheleno/consultagit/GitHubService/view/MainActivity.kt
@@ -0,0 +1,24 @@
+package br.com.marcioheleno.consultagit.GitHubService.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.marcioheleno.consultagit.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val retrofit = configuraRetroFilt()
+
+//        executaRequisicao(retrofit)
+
+//        val retrofit
+//        val businessLogic = BusinessLogic()
+
+    }
+
+
+}
